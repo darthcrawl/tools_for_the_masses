193c60c4f9a4f251841d6771df83f235134e56ee
darthcrawl <156199874+darthcrawl@users.noreply.github.com>
2024-05-02 08:14:59 -0400

    Add files via upload
    Syntax error fixed. No idea how a "g" got in the code, but it's gone now.

--- webp converter/webp_converter.py
+++ webp converter/webp_converter.py
@@ -7,7 +7,7 @@ directory = os.path.dirname(os.path.realpath(__file__))
 for filename in os.listdir(directory):
     if filename.endswith('.webp'):
         with Image.open(os.path.join(directory, filename)) as img:
-            jpg_filename = filename[:-5] + '.jpg'g
+            jpg_filename = filename[:-5] + '.jpg'
             img.convert('RGB').save(os.path.join(directory, jpg_filename), 'JPEG')
 
 print("Conversion completed.")
